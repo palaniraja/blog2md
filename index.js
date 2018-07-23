4cce5da77c3b2f5fbd5aacf982be38259589f2ae
palaniraja <palaniraja@live.com>
2018-07-23 20:27:46 +0530

    blogger comment parsing working version

--- index.js
+++ index.js
@@ -10,9 +10,9 @@
     - [x] Parse Title, Link, Created, Updated, Content, Link
     - [ ] List Post & Respective comment counts
     - [x] Content to MD - pandoc?
-    - [ ] Parse Images, Files, Videos linked to the posts
+    - [-] Parse Images, Files, Videos linked to the posts
     - [x] Create output dir
-    - [ ] List items that are not downloaded( or can't) along with their .md file for user to proceed
+    - [-] List items that are not downloaded( or can't) along with their .md file for user to proceed
 
 
 */
@@ -81,9 +81,9 @@ function wordpressImport(backupXmlFile, outputDir){
                 return 1;
             }
             // console.dir(result); 
-            // console.log(JSON.stringify(result));
+            // console.log(JSON.stringify(result)); return;
             var posts = [];
-
+            
             // try {
                 posts = result.rss.channel[0].item;
                 
@@ -110,9 +110,11 @@ function wordpressImport(backupXmlFile, outputDir){
                 var fname = '';
                 var markdown = '';
                 var fileContent = '';
+                
                 posts.forEach(function(post){
                     
                     title = post.title[0];
+                    
                     // console.log(title);
 
                     // if (title && title.indexOf("'")!=-1){
@@ -155,14 +157,14 @@ function wordpressImport(backupXmlFile, outputDir){
 
                     fname = outputDir+'/'+fname+'.md';
                     console.log(`fname: '${fname}'`);
-
+                    
                     if (post["content:encoded"]){
                         // console.log('content available');
                         content = '<div>'+post["content:encoded"]+'</div>'; //to resolve error if plain text returned
                         markdown = tds.turndown(content);
                         // console.log(markdown);
 
-                        fileContent = `---\ntitle: '${title}'\ndate: ${published}\ndraft: false\n${tagString}---\n${markdown}`;
+                        fileContent = `---\ntitle: '${title}'\ndate: ${published}\ndraft: false\n${tagString}---\n\n${markdown}`;
 
                         writeToFile(fname, fileContent);
                         
@@ -194,10 +196,6 @@ function bloggerImport(backupXmlFile, outputDir){
                 console.log(`Total no. of entries found : ${contents.length}`);
                 // var i=0
                 var posts = contents.filter(function(entry){
-                    // if (entry['thr:in-reply-to']){
-                    //     i++;
-                    //     return false;
-                    // }
                     return entry.id[0].indexOf('.post-')!=-1 && !entry['thr:in-reply-to']
                 });
 
@@ -213,18 +211,18 @@ function bloggerImport(backupXmlFile, outputDir){
                  var content = '';
                  var markdown = '';
                  var fileContent = '';
+                 var postMaps = {};
 
                 posts.forEach(function(entry){
+                    var postMap = {};
+                    
                     var title = entry.title[0]['_'];
                     // title = tds.turndown(title);
                     if (title && title.indexOf("'")!=-1){
                          title = title.replace(/'/g, "''");
                     }
-                    
-                    // title = title.replace(/'/g, "\\'");
-                    // title = title.replace(/:/g, "\\:");
-                    // title = title.replace(/\[/g, '\\[');
-                    // title = title.replace(/\]/g, '\\]');
+                    postMap.pid = entry.id[0].split('-').pop()
+
                     var published = entry.published;
                     var draft = 'false';
 
@@ -246,6 +244,9 @@ function bloggerImport(backupXmlFile, outputDir){
                         var fname = outputDir + '/' + path.basename(url);
                         fname = fname.replace('.html', '.md')
                         console.log(fname);
+                        postMap.fname = fname.replace('.md', '-comments.md');
+                        postMap.comments = [];
+                        postMaps[postMap.pid] = postMap;
 
                         if (entry.content && entry.content[0] && entry.content[0]['_']){
                             // console.log('content available');
@@ -277,16 +278,72 @@ function bloggerImport(backupXmlFile, outputDir){
                             tagString=`tags : [${tags.join(', ')}]\n`;
                         }
 
+                        console.dir(postMap);
+
                         console.log("\n\n\n\n\n");
 
 
-                        fileContent = `---\ntitle: '${title}'\ndate: ${published}\ndraft: false\n${tagString}---\n${markdown}`;
+                        fileContent = `---\ntitle: '${title}'\ndate: ${published}\ndraft: false\n${tagString}---\n\n${markdown}`;
 
-                        writeToFile(fname, fileContent)
+                        // writeToFile(fname, fileContent)
 
                     }
                     
                 });
+
+
+            comments.forEach(function(entry){
+                // var commentMap = {};
+                var comment = {published:'', title:'', content:''};
+
+                var postId = entry['thr:in-reply-to'][0]["$"]["source"];
+                postId = path.basename(postId);
+
+                comment.published = entry['published'][0];
+
+                if(entry['title'][0] && entry['title'][0]["_"]){
+                    comment.title = tds.turndown(entry['title'][0]["_"]);    
+                }
+
+                if (entry['content'][0] && entry['content'][0]["_"]){
+                    comment.content = tds.turndown(entry['content'][0]["_"]);    
+                }
+                
+                comment.author = {name: '', email: '', url: ''};
+                
+                if(entry['author'][0]["name"] && entry['author'][0]["name"][0]){
+                    comment.author.name = entry['author'][0]["name"][0];    
+                }
+                
+                if (entry['author'][0]["email"] && entry['author'][0]["email"][0]){
+                    comment.author.email = entry['author'][0]["email"][0];    
+                }
+                
+                if (entry['author'][0]["uri"] && entry['author'][0]["uri"][0]){
+                    comment.author.url = entry['author'][0]["uri"][0];    
+                }
+                
+                postMaps[postId].comments.push(comment);
+            });
+
+            // console.log(JSON.stringify(postMaps)); return;
+           
+            for (var pmap in postMaps){
+                var comments = postMaps[pmap].comments;
+                console.log(`post id: ${pmap} has ${comments.length} comments`);
+                // console.dir(comments);
+
+                if (comments.length){
+                    var ccontent = '';
+                    comments.forEach(function(comment){
+                        ccontent += `#### ${comment.title}\n[${comment.author.name}](${comment.author.url} "${comment.author.email}") - ${comment.published}\n\n${comment.content}\n<hr />\n`;
+                    });
+
+                    writeToFile(postMaps[pmap].fname, ccontent);
+                }
+            }
+
+
             }
             console.log('Done');
         });
